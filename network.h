082886165ffc4022830eb7af78d254f34b5acc9c
aesin <alexey.aesin@gmail.com>
2015-01-22 02:12:04 +0300

    network?

---
+++ network.h
@@ -0,0 +1,13 @@
+#ifndef network_H
+#define network_H
+
+namespace settlers {
+enum PackageType{GAME_EVENT, PACKAGE_ACCEPT, GAME_STATE, PACKAGE_TYPE_NUMBER};
+
+class GameEvent{
+
+};
+
+
+}
+#endif // network_H
